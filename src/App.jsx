18a67daaab4b2853801f5c169476092e117cf9a0
alimohamed33 <alimohamedali2110@gmail.com>
2023-09-11 07:19:03 +0300

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+function App() {
+  const H1 = styled.h1`
+    font-size: 2rem;
+    font-weight: 600;
+    background-color: yellowgreen;
+  `;
+
+  return (
+    <div>
+      <H1>Test</H1>
+    </div>
+  );
+}
+export default App;
